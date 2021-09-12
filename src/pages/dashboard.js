3740eb63e669b94b95e60e477d96592f2d987e30
Julián Swerdlin <julianswer36@gmail.com>
2021-09-12 19:55:12 -0300

    Update dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import PropTypes from 'prop-types';
 import Header from '../components/header';
 import Timeline from '../components/timeline';
